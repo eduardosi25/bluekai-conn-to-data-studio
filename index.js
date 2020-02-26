bbabf24e71ee6c1f6af8326874ab42e0434b4252
eduardosi25 <eduardosi25@hotmail.com>
2020-02-26 11:32:10 -0600

    fix to connection on prod

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ dotenv.config({path:'./config/config.env'});
 const PORT = process.env.PORT || 3000;
 const DB_DOMAIN = process.env.DB_DOMAIN;
 const DB_DATABASE = process.env.DB_DATABASE;
-
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_TABLE = process.env.DB_TABLE;
 //conexion base de datos 
 mongoose.promise = global.promise;
-mongoose.connect( `mongodb://${DB_DOMAIN}:27017/${DB_DATABASE}`,{ useUnifiedTopology: true,  useNewUrlParser: true, useFindAndModify: false})
+mongoose.connect( `mongodb+srv://${DB_USER}:${DB_PASSWORD}@audiencekit1-76o4f.mongodb.net/${DB_TABLE}?retryWrites=true&w=majority`,
+                  { useUnifiedTopology: true,  useNewUrlParser: true, useFindAndModify: false})
 .then (()=>{
     console.log("la conexion esta lista")
     //Crear servidor 
