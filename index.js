9f6148f8eb3e2240376efd7b2ef15383ed2a9887
eduardosi25 <eduardosi25@hotmail.com>
2020-02-28 16:29:56 -0600

    variables for production or development on docker compose

--- index.js
+++ index.js
@@ -10,18 +10,40 @@ const DB_DATABASE = process.env.DB_DATABASE;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_TABLE = process.env.DB_TABLE;
+const NODE_ENV = process.env.NODE_ENV;
 //conexion base de datos 
 mongoose.promise = global.promise;
-// mongoose.connect( `mongodb+srv://appaudiencekit:aEqslb71Vh0DUVkD@audiencekit1-76o4f.mongodb.net/audienceKit?retryWrites=true&w=majority`,
+// mongoose.connect(`mongodb+srv://appaudiencekit:aEqslb71Vh0DUVkD@audiencekit1-76o4f.mongodb.net/audienceKit?retryWrites=true&w=majority`,
 //                   {useNewUrlParser: true, useUnifiedTopology: true })
 
-
-mongoose.connect( `mongodb+srv://${DB_USER}:${DB_PASSWORD}@audiencekit1-76o4f.mongodb.net/${DB_TABLE}?retryWrites=true&w=majority`,
-                   { useUnifiedTopology: true,  useNewUrlParser: true, useFindAndModify: false})
+//  mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@audiencekit1-76o4f.mongodb.net/${DB_TABLE}?retryWrites=true&w=majority`,
+//                    { useUnifiedTopology: true,  useNewUrlParser: true, useFindAndModify: false})
+if(NODE_ENV === "production")
+{
+ mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@audiencekit1-76o4f.mongodb.net/${DB_TABLE}?retryWrites=true&w=majority`,                    
+      { useUnifiedTopology: true,  useNewUrlParser: true, useFindAndModify: false})
 .then (()=>{
-    console.log("la conexion esta lista")
+    console.log("la conexion esta lista production")
     //Crear servidor 
     app.listen(PORT, ()=>{
       console.log(`servidor corriendo en ${process.env.NODE_ENV} mode on port ${PORT}`)
     });
 }).catch(err => console.log(err));
+}
+else if(NODE_ENV === "development")
+{
+mongoose.connect(`mongodb://mongodb:27017/audienceKit`,
+                    { useUnifiedTopology: true,  useNewUrlParser: true, useFindAndModify: false})
+.then (()=>{
+    console.log("la conexion esta lista desarrollo")
+    //Crear servidor 
+    app.listen(PORT, ()=>{
+      console.log(`servidor corriendo en ${process.env.NODE_ENV} mode on port ${PORT}`)
+    });
+}).catch(err => console.log(err));
+
+}
+console.log(NODE_ENV); //"production"
+
+  
+ 
