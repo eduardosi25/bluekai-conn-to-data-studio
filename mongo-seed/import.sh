3c115d01151381bb1d487c21933664e5c96dc7e2
eduardosi25 <eduardosi25@hotmail.com>
2020-08-13 13:17:54 -0500

    erase details

--- mongo-seed/import.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-mongoimport --host mongodb --db audienceKit --collection segments --type json --file /mongo-seed/segments.json
-mongoimport --host mongodb --db audienceKit --collection users --type json --file /mongo-seed/users.json
