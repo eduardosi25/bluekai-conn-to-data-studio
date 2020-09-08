3438efb2939d6c8aff0827cbbad27ffe5144a6f5
eduardosi25 <eduardosi25@hotmail.com>
2020-09-08 12:00:40 -0500

    awth para report in date

--- routes/report.js
+++ routes/report.js
@@ -8,7 +8,7 @@ var md_awth = require('../middlewares/authenticated');
 
 api.get('/report/audienceId/:id', ReportController.getReportByAudId);       //lista de report por id de audiencia
 api.get('/report/date/:clientId?/:date?', ReportController.getReportByDate);       //lista de report por fecha de segmento
-api.get('/report/dates/:clientId?/:date/:date2', ReportController.getReportInDate);       //lista de report por fecha de segmento
+api.get('/report/dates/:clientId?/:date/:date2',  md_awth.ensureAuth, ReportController.getReportInDate);       //lista de report por fecha de segmento
 api.get('/report/clientId/:id', ReportController.getReportByCliId);       //lista de report por id de cliente
 api.get('/report/audName/:clientId?/:audName', ReportController.getReportByAudName);
 api.get('/report/catName/:clientId?/:catName?', ReportController.getReportByCatName);
